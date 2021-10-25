5abde83ee8ed0ab36c40fcf7f4e198ec6d56d87f
Santiago <santiagojavierveiga@gmail.com>
2021-10-25 15:52:46 -0300

    feat(client/About): shows about's page

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About (){
+    return (
+        <div>
+            <h2>ABOUT</h2>
+            <p>Página para ver el clima de distintas ciudades</p>
+        </div>
+    );
+};
