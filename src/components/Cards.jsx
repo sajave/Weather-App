5a79acb99ad2d31f9572ae82a4007b18ab76713a
Santiago <santiagojavierveiga@gmail.com>
2021-10-25 23:05:01 -0300

    fix(client/Cards): fix flex grid

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,18 +5,20 @@ import Card from "./Card.jsx";
 
 export default function Cards({cities, onClose}) {
   return (
-    <div className={styles.cards}>
-      {cities.map((c) => (
-        <Card
-          key={c.id}
-          max={c.max}
-          min={c.min}
-          name={c.name}
-          img={c.img}
-          onClose={() => onClose(c.id)}
-          id={c.id}
-        />
-      ))}
+    <div className={styles.wrapper}>
+      <div className={styles.cards}>
+        {cities.map((c) => (
+          <Card
+            key={c.id}
+            max={c.max}
+            min={c.min}
+            name={c.name}
+            img={c.img}
+            onClose={() => onClose(c.id)}
+            id={c.id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
